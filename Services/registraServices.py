8941db712aa5b22f50619b0915076e9fc1eb563f
sebas604 <72045587+sebas604@users.noreply.github.com>
2026-09-11 14:38:47 -0500

    se a areglado registra

--- Services/registraServices.py
+++ Services/registraServices.py
@@ -1,36 +1,12 @@
 from flask import current_app, jsonify
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 from Models.registra import registra
 import uuid
 
 class registraServer:
 
     def add(cInfo):
-<<<<<<< Updated upstream
         uuid_reg = uuid.uuid4()
-=======
         uuid_reg= uuid.uuid4()
->>>>>>> Stashed changes
-        c = current_app.mysql.connection.cursor()
-        query = """INSERT INTO t_registra 
-        (reg_uuid, reg_usu_id, reg_com_id)
-        VALUES (%s, %s, %s)"""
-        c.execute(query, (uuid_reg,))
-
-
-from Models.registra import registra
-
-from Models.registra import registra
-
-import uuid
-
-class registraServer:
-
-    def add(cInfo):
-        uuid_reg = uuid.uuid4()
         c = current_app.mysql.connection.cursor()
         query = """INSERT INTO t_registra 
         (reg_uuid, reg_usu_id, reg_com_id)
@@ -73,26 +49,18 @@ class registraServer:
     def read():
         c = current_app.mysql.connection.cursor()
 
-<<<<<<< Updated upstream
+
         query = "SELECT * FROM t_registra"
         c.execute(query)
         data = c.fetchall()
         regg = [registra(row[0], row[1], row[2], row[3]).to_dict() for row in data]
 
-=======
->>>>>>> Stashed changes
+
         query = "SELECT * FROM t_registra"
         c.execute(query)
         data = c.fetchall()
         reg = [registra(row[0], row[1], row[2], row[3]).to_dict() for row in data]
 
-<<<<<<< Updated upstream
-=======
-        query = "SELECT * FROM t_registra"
-        c.execute(query)
-        data = c.fetchall()
-        reg = [registra(row[0], row[1], row[2], row[3]).to_dict() for row in data]
 
->>>>>>> Stashed changes
         print(data)
         
